0e37e2ef29f5ae6f36123ca534c53d5f28f518e5
Lonewolftotheworld <2556918@students.wits.ac.za>
2025-03-10 16:49:43 +0200

    Update script.js
    reduced the flags size

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ document.getElementById('submit-country').addEventListener('click', function () 
                 <strong>Region:</strong> ${region}<br>
                 <strong>Population:</strong> ${population}<br>
                 <strong>Flag:</strong><br>
-                <img src="${flag}" alt="${country.name.common} flag" width="300px">
+                <img src="${flag}" alt="${country.name.common} flag" width="200px">
             `;
 
             
@@ -41,7 +41,7 @@ document.getElementById('submit-country').addEventListener('click', function () 
 
                             borderList.innerHTML += `
                                 <p>
-                                    <img src="${borderFlag}" alt="${borderName} flag" width="100px">
+                                    <img src="${borderFlag}" alt="${borderName} flag" width="50px">
                                     ${borderName}
                                 </p>
                             `;
